4cdff08845e71adf32d06b8712d1ede12b28fe9a
Akongnwi Che Devert <akongnwi.devert@cm.maviance.com>
2019-09-09 12:45:40 +0100

    Fixing timeout bug

--- app/Services/HexcellClient.php
+++ app/Services/HexcellClient.php
@@ -118,12 +118,6 @@ class HexcellClient
     {
         return new \GuzzleHttp\Client([
             'cookies' => true,
-            'headers' => [
-                'Accept'                    => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
-                'Accept-Encoding'           => 'gzip, deflate',
-                'Accept-Language'           => 'en,fr;q=0.9,en-US;q=0.8',
-                'User-Agent'                => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-            ]
         ]);
     }
 
